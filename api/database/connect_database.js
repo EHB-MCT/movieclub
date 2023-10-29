440084f994c40d0613cd7aa4aae04aa11c77f87c
RhysDevalckeneer2 <rhys.devalckeneer@hotmail.be>
2023-10-29 08:03:22 +0100

    feat(database): Implement connect_database with Knex setup

---
+++ api/database/connect_database.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+
+const knex = require('knex');
+const knexFile = require('./knexfile.js');
+
+const environment = process.env.NODE_ENV || "development";
+
+module.exports = knex(knexFile[environment]);
