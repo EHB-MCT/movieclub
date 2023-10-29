a497e0f04d57b4e6be314ac980e6310e29aa10e8
RhysDevalckeneer2 <rhys.devalckeneer@hotmail.be>
2023-10-29 11:39:52 +0100

    chore(migration): Initialize create_lists_table migration

---
+++ api/database/migrations/20231029103644_create_lists_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('lists', (table) => {
+        table.increments('id').primary(); // Primary key
+        table.text('name').notNullable();
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('lists');
+};
