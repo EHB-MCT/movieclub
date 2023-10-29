58d07d9b2ff368ef0b3c294f4b4b2d2b833cd5fe
RhysDevalckeneer2 <rhys.devalckeneer@hotmail.be>
2023-10-29 09:07:08 +0100

    chore(migration): Temporarily comment out sections in create_movies_genres_table

--- api/database/migrations/20231029072420_create_movies_genres_table.js
+++ api/database/migrations/20231029072420_create_movies_genres_table.js
@@ -5,8 +5,8 @@
 exports.up = function(knex) {
     return knex.schema.createTable('movies_genres', (table) => {
         table.increments('id').primary(); // Primary key
-        table.integer('movie_id').unsigned().references('movies.id').inTable('movies');
-        table.integer('genre_id').unsigned().references('genres.id').inTable('genres');
+        // table.integer('movie_id').unsigned().references('movies.id').inTable('movies');
+        // table.integer('genre_id').unsigned().references('genres.id').inTable('genres');
         table.timestamp('created_at').defaultTo(knex.fn.now());
         table.timestamp('updated_at').defaultTo(knex.fn.now());
     });
