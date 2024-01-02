f380a72d4dffba14b1641058671cae8ecbe43a72
RhysDevalckeneer2 <44397726+RhysDevalckeneer2@users.noreply.github.com>
2024-01-02 10:02:12 +0100

    feat(genres): add test for genres

---
+++ api/__tests__/unit/genres/getGenreById.test.js
@@ -0,0 +1,23 @@
+const request = require('supertest');
+const express = require('express');
+const router =  require('../../../routes/genres.route');
+const genres = require('../../../controllers/genre.controller');
+
+const app = express();
+
+app.use('/api/v1/genres', router);
+
+describe('GET /api/v1/genres/:id', () => {
+    it('should return the genre with a specific ID', async () => {
+        const exampleGenre = { id: 1, name: 'Action' };
+        
+        //genres.getGenreById.mockResolvedValueOnce(exampleGenre);
+        jest.spyOn(genres, 'getGenreById').mockResolvedValueOnce(exampleGenre);
+
+
+        const response = await request(app).get('/api/v1/genres/1');
+
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(exampleGenre);
+    })
+})
