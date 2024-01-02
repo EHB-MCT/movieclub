bebb03191f526137b288f4e183d64e984aae8ec8
RhysDevalckeneer2 <44397726+RhysDevalckeneer2@users.noreply.github.com>
2024-01-02 09:58:59 +0100

    refactor: delete frontentd, backend focus

--- client/src/app/movies/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Page = () => {
-  return (
-    <div>movies</div>
-  )
-}
-
-export default Page
--- client/src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import Image from 'next/image'
-
-export default function Page() {
-  return (
-    <div>homepage</div>
-  )
-}
