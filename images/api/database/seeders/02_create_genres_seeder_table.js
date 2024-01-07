b81b95ab4eca63fefa01c8792ae043891764ef67
RhysDevalckeneer2 <44397726+RhysDevalckeneer2@users.noreply.github.com>
2024-01-07 10:22:15 +0100

    feat(seeder): edit seeder run

--- images/api/database/seeders/02_create_genres_seeder_table.js
+++ images/api/database/seeders/02_create_genres_seeder_table.js
@@ -3,30 +3,35 @@
  * @returns { Promise<void> } 
  */
 exports.seed = async function(knex) {
-  await knex('genres').insert([
-    { name: 'action' },
-    { name: 'adventure' },
-    { name: 'animation' },
-    { name: 'biography' },
-    { name: 'comedy' },
-    { name: 'crime' },
-    { name: 'documentary' },
-    { name: 'drama' },
-    { name: 'family' },
-    { name: 'fantasy' },
-    { name: 'film noir' },
-    { name: 'history' },
-    { name: 'horror' },
-    { name: 'music' },
-    { name: 'musical' },
-    { name: 'mystery' },
-    { name: 'romance' },
-    { name: 'sci-fi' },
-    { name: 'short film' },
-    { name: 'sport' },
-    { name: 'superhero' },
-    { name: 'thriller' },
-    { name: 'war' },
-    { name: 'western' }
-  ]);
+  let data = await knex('genres');
+  if(data.length === 0) {
+    return knex('genres').insert([
+      { name: 'action' },
+      { name: 'adventure' },
+      { name: 'animation' },
+      { name: 'biography' },
+      { name: 'comedy' },
+      { name: 'crime' },
+      { name: 'documentary' },
+      { name: 'drama' },
+      { name: 'family' },
+      { name: 'fantasy' },
+      { name: 'film noir' },
+      { name: 'history' },
+      { name: 'horror' },
+      { name: 'music' },
+      { name: 'musical' },
+      { name: 'mystery' },
+      { name: 'romance' },
+      { name: 'sci-fi' },
+      { name: 'short film' },
+      { name: 'sport' },
+      { name: 'superhero' },
+      { name: 'thriller' },
+      { name: 'war' },
+      { name: 'western' }
+    ]);
+  } else { 
+    return; 
+  };
 };
